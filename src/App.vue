9e01473e3c67ba62de80020032400b03edf3b10e
Mariosam2 <marco.mariosaa@gmail.com>
2022-11-16 12:57:11 +0100

    milestone 1

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,50 @@
 <script>
+import { store } from './store.js';
+import axios from 'axios';
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      store,
+    }
+  },
+  methods: {
+    getCurrentMovies() {
+      const config = {
+        method: 'get',
+        url: 'https://api.themoviedb.org/3/search/movie',
+        params: {
+          api_key: this.store.API_KEY,
+          query: this.store.queryString,
+        }
+      }
+
+      axios(config)
+        .then(resp => {
+          console.log((resp.data));
+          this.store.currentMovies = resp.data.results;
+          console.log(this.store.currentMovies);
+        })
+        .catch(error => {
+          console.log(error);
+        })
+    }
+  },
+  mounted() {
+    this.getCurrentMovies()
+  }
 }
 </script>
 
-<template></template>
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col" v-for="movie in store.currentMovies">
+        <div class="movie-title">{{ movie.title }}</div>
+        <div class="original-title">{{ movie.original_title }}</div>
+        <div class="lang">{{ movie.origianl_language }}</div>
+        <div class="vote-count">{{ movie.vote_count }}</div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/store.js
@@ -0,0 +1,10 @@
+import { reactive } from "vue";
+
+export let store = reactive({
+    API_KEY: '8223b7e6f75caa7d554c0aa366a0c2e3',
+    queryString: 'ritorno al fut',
+    currentMovies: null,
+    /* callApi(url) {
+
+    } */
+})
